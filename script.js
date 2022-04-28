63c5ba71ccdbafda5ed3cc5d4df0f8b3c59a7622
İsmail KURT <78919551+Sephry@users.noreply.github.com>
2022-04-29 00:19:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function select(el) {
+    return document.querySelector(el);
+}
+
+let qrcode = select("img");
+let qrtext = select("textarea");
+let qrbutton = select("button");
+
+qrbutton.addEventListener("click", generateQR)
+
+function generateQR() {
+    let size = "1000x1000";
+    let data = qrtext.value;
+    let baseURL = "http://api.qrserver.com/v1/create-qr-code/";
+
+    let url = `${baseURL}?data=${data}&size=${size}`;
+
+    qrcode.src = url;
+}
